4e9c9860c010904049a92986153b826f83c881a7
Arnaud Lheureux <arnaudlh@users.noreply.github.com>
2024-01-24 03:37:42 +0000

    provider updates

--- main.tf
+++ main.tf
@@ -3,14 +3,14 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.75.0"
+      version = "~> 3.88.0"
       configuration_aliases = [
         azurerm.vhub
       ]
     }
     azuread = {
       source  = "hashicorp/azuread"
-      version = "~> 2.43.0"
+      version = "~> 2.47.0"
     }
     azapi = {
       source  = "azure/azapi"
